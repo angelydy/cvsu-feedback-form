2ccea287f6691021bf2618d1014336405ea32b27
angely <angelymaedyy@gmail.com>
2022-12-09 15:27:26 +0800

    updates form

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
-function validateForm() {
-  // Get all the radio buttons
-  var radioButtons = document.querySelectorAll('input[type="radio"]');
-  // Loop through the radio buttons and check if any of them are selected
-  var selected = false;
-  for (var i = 0; i < radioButtons.length; i++) {
-    if (radioButtons[i].checked) {
-      selected = true;
-      break;
-    }
-  }
-  // If none of the radio buttons are selected, show an error message and prevent the form from being submitted
-  if (!selected) {
-    alert('Please select an option for each criteria.');
-  }
-  else {
-    showNextPage();
-  }
-}
-
 var currentQuestion = 1;
-
 function showNextPage() {
   //Hide the current question  
   document.getElementById("page-" + currentQuestion).style.display = "none";
