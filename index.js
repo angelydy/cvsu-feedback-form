994d4e9f89a3e7f2c1e3a9c5c7234931797e50c8
angely <angelymaedyy@gmail.com>
2022-12-10 20:48:19 +0800

    added modal

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 var currentQuestion = 1;
+function submitBtnClick() {
+  // Show the modal
+  document.getElementById("myModal").style.display = "block";
+  //blur the background
+  document.getElementById("body-container").style.filter = "blur(5px)";
+}
+
+function noBtnClick() {
+  // Hide the modal
+  document.getElementById("myModal").style.display = "none";
+  
+  document.getElementById("body-container").style.filter = "blur(0px)";
+}
+
+function yesBtnClick() {
+  // Hide the modal
+  document.getElementById("myModal").style.display = "none";
+  // Hide the current question
+  document.getElementById("page-" + currentQuestion).style.display = "none";
+  // Show the first question
+  currentQuestion = 1;
+  document.getElementById("page-" + currentQuestion).style.display = "block";
+  //submit the form
+  document.getElementById("questionnaire-form").submit();
+}
+
 function showNextPage() {
   //Hide the current question  
   document.getElementById("page-" + currentQuestion).style.display = "none";
   // Show the next question
   currentQuestion++;
-  document.getElementById("page-" + currentQuestion).style.display = "block";
-  
+  document.getElementById("page-" + currentQuestion).style.display = "block";  
 }
 
 function showPreviousPage() {
@@ -16,3 +41,5 @@ function showPreviousPage() {
   currentQuestion--;
   document.getElementById("page-" + currentQuestion).style.display = "block";
 }
+
+
